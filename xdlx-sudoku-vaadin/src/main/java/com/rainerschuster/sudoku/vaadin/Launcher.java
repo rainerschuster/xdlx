cedb35252b5fbc3e7ccf95e8f8ada8962c98c0d6
Rainer Schuster <rainer.schuster@gmail.com>
2014-11-18 22:03:22 +0100

    Trying heroku support.

---
+++ xdlx-sudoku-vaadin/src/main/java/com/rainerschuster/sudoku/vaadin/Launcher.java
@@ -0,0 +1,12 @@
+package com.rainerschuster.sudoku.vaadin;
+
+import static eu.hurion.vaadin.heroku.VaadinForHeroku.forApplication;
+import static eu.hurion.vaadin.heroku.VaadinForHeroku.herokuServer;
+
+public class Launcher {
+
+    public static void main(final String[] args) {
+        herokuServer(forApplication(SudokuUI.class)).start();
+    }
+
+}
