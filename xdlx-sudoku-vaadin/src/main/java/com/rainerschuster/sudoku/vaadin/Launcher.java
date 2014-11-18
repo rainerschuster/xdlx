08f4222d8b83284ccfa7f41617ba36a85f7c071b
Rainer Schuster <rainer.schuster@gmail.com>
2014-11-18 22:31:24 +0100

    withoutMemcachedSessionManager

--- xdlx-sudoku-vaadin/src/main/java/com/rainerschuster/sudoku/vaadin/Launcher.java
+++ xdlx-sudoku-vaadin/src/main/java/com/rainerschuster/sudoku/vaadin/Launcher.java
@@ -6,7 +6,7 @@ import static eu.hurion.vaadin.heroku.VaadinForHeroku.herokuServer;
 public class Launcher {
 
     public static void main(final String[] args) {
-        herokuServer(forApplication(SudokuUI.class)).start();
+        herokuServer(forApplication(SudokuUI.class)).withoutMemcachedSessionManager().start();
     }
 
 }
